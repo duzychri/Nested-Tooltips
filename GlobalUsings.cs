16b1b75094f171e057cb565b8452039e8a72a146
Christoph Duzy <duzychri@users.noreply.github.com>
2025-07-05 00:44:31 +0200

    Added GlobalSuppression and GlobalUsings files

---
+++ GlobalUsings.cs
@@ -0,0 +1,17 @@
+global using Godot;
+
+global using System;
+global using System.IO;
+global using System.Linq;
+global using System.Text;
+global using System.Text.Json;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using System.Diagnostics;
+global using System.Collections;
+global using System.Globalization;
+global using System.Collections.Frozen;
+global using System.Collections.Generic;
+global using System.Collections.Immutable;
+global using System.Collections.ObjectModel;
+global using System.Diagnostics.CodeAnalysis;
