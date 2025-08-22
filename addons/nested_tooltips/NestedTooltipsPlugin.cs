29e58b638336c00485ddcc719a346d1a6d18735f
Christoph Duzy <duzychri@users.noreply.github.com>
2025-08-22 11:23:11 +0200

    Converted project into addon

---
+++ addons/nested_tooltips/NestedTooltipsPlugin.cs
@@ -0,0 +1,20 @@
+#if TOOLS
+using Godot;
+using System;
+
+[Tool]
+public partial class NestedTooltipsPlugin : EditorPlugin
+{
+    private const string Autoload_Prefab_Name = "TooltipServicePrefab";
+
+    public override void _EnterTree()
+    {
+        AddAutoloadSingleton(Autoload_Prefab_Name, "res://addons/nested_tooltips/Prefabs/TooltipService_Prefab.tscn");
+    }
+
+    public override void _ExitTree()
+    {
+        RemoveAutoloadSingleton(Autoload_Prefab_Name);
+    }
+}
+#endif
